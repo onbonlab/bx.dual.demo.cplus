6d82e008648a4085a4aeccf262555f272c3e081a
jiqiubo <jiqb@onbonbx.com>
2020-02-11 19:25:03 +0800

    创建 win32 Demo;

---
+++ bx.dual.sdk.demo.win/LedSdkDemo/LedSdkDemo.cpp
@@ -0,0 +1,27 @@
+﻿// LedSdkDemo.cpp : 定义控制台应用程序的入口点。
+
+#include "stdafx.h"
+#include "onbonTest.h"
+
+#include <windows.h>
+#include <stdio.h>
+
+//#pragma comment(lib,"../Debug/LedEQSdk.lib")
+//#pragma comment(lib,"../x64/Release/LedEQSdk.lib")
+
+int main(int argc, _TCHAR* argv[])
+{
+	printf("welcome to vs ... \n");
+
+	fantx_onbonTest();
+	//这里可以调用别人的测试函数
+	
+
+	while(1){
+		Sleep(1000);
+		;
+	}
+
+	return 0;
+}
+
